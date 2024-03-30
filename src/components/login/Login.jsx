e37d65f6c273829de776741c496e845dd1f9f92e
Vincent Gagnon <gagnon0vincent@gmail.com>
2024-03-30 15:00:04 -0400

    added backend login route

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -5,17 +5,57 @@ function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-type' : 'application/json'
+        },
+        body: JSON.stringify({email, password})
+      });
+
+      if(!response.ok) {
+        throw new Error(`Http error status: ${response.status}`);
+      }
+
+      // Parse response as text
+      const token = await  response.text();
+      // For dev purposes
+      console.log(token);
+      // store token
+      localStorage.setItem("token", token);
+
+    } catch (error) {
+      console.error('There was a problem with the login request:', error);
+      // TODO: handle error (err msg to user)
+    }
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>Email: </div>
-        <input type="text" value={email}></input>
+        <input
+            name="email"
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+        />
 
         <div>Password: </div>
-        <input type="text" value={password}></input>
+        <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type={"submit"}>Login</button>
       </form>
 
-      
+
     </div>
   )
 }
