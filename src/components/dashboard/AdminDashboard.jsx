55a0a6b40f78e0567c211bfe09c0a27b297536c6
Yufeng Liu <yufeng.liu15@gmail.com>
2024-04-08 10:30:44 -0400

    added css styling

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import "./dashboard.css";
 
 function AdminDashboard() {
     useEffect(() =>{
--- src/components/dashboard/memberDashboard.jsx
+++ src/components/dashboard/memberDashboard.jsx
@@ -9,8 +9,10 @@ import {
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
+import "./dashboard.css";
+
 const memberDashboard = () => {
-    const currentDate = '2018-11-01';
+    const currentDate = document.getDate;
 const schedulerData = [
   { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
   { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
